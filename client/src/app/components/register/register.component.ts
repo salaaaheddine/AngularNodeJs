eb9c1a824ba85006ac18b38faee9ee85d7e3e2b4
Salah <theonegreat0@gmail.com>
2024-05-15 07:04:11 +0100

    Register updated: form validation, routing

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthServiceService } from 'src/app/services/auth-service.service';
 
 @Component({
   selector: 'app-register',
@@ -7,20 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  fullName = '';
-  email = '';
-  password = '';
-  errorMessage = '';
+
+
+  form !: FormGroup;
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private auth: AuthServiceService
+  ) {
+  }
 
   ngOnInit(): void {
-      
+    this.form = this.formBuilder.group({
+      fullname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4)]]
+    })
   }
 
-  onSubmit() {
-    // Implement your login logic here
-    // This example just simulates successful login
-    this.fullName = '';
-    this.password = '';
-    // You might redirect to another page or perform other actions
+
+
+  onSubmit(): void {
+    console.log("Submitted")
+    this.auth.register(this.form.getRawValue())
   }
+
+  getEmailErrorMessage() : string {
+    if (this.form.get('email')!.errors!['required']) 
+      return 'Email is required.'
+    else if (this.form.get('email')!.errors!['email']) 
+      return 'Invalid email format.';
+    return ""
+  }
+    getPasswordErrorMessage() {
+      if (this.form.get('password')!.errors!['required']) 
+        return 'Password is required.'
+      else if (this.form.get('password')!.errors!['minlength']) 
+        return 'at least 4 characters long.'
+      return ''
+    }
+
 }
