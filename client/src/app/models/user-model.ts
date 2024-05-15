77fd4d267659bfad81d4f09786b7040c18d842d3
Salah <theonegreat0@gmail.com>
2024-05-15 07:00:42 +0100

    User Model added

---
+++ client/src/app/models/user-model.ts
@@ -0,0 +1,10 @@
+export interface RegistrationData {
+    fullname: string;
+    email: string;
+    password: string;
+  }
+  
+  export interface LoginData {
+    email: string;
+    password: string;
+  }
